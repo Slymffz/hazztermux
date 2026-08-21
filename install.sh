eec099d270e2f256f8fdae1efdfff8414811de6b
Slymffz <10ryanbarros@gmail.com>
2026-08-20 23:08:33 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -u
 
-REPO_URL="${https://github.com/Slymffz/hazztermux}"
+REPO_URL="${https://github.com/Slymffz/hazztermux.git}"
 INSTALL_DIR="${HAZZSCREENS_DIR:-$HOME/hazztermux}"
 
 say() {
